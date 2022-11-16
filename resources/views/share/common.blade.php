e37c2a81f928e586345f5f22b72f56586456c6c1
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-16 17:54:32 +0600

    ok

--- resources/views/share/common.blade.php
+++ resources/views/share/common.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/e21bebfc6d.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('assets/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/style.css')}}">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title')</title>
   </head>
