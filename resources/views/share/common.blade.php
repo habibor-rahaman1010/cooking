f4c56b3a204cdfc49cd551baf3f6f077769ce4b1
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-18 23:10:17 +0600

    ok done

--- resources/views/share/common.blade.php
+++ resources/views/share/common.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="{{secure_asset('assets/style.css')}}">
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    <link rel="stylesheet" href="{{secure_asset('public/build/manifest.json')}}">
+    {{-- <link rel="stylesheet" href="{{secure_asset('public/build/manifest.json')}}"> --}}
     @vite('resources/css/app.css')
 
     <title>@yield('title')</title>
