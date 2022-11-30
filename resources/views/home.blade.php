43c6a0836c6bf53ff4c8f7eca9d4aa6fd5aa58ad
habibor144369 <onilhabib1@gmail.com>
2022-12-01 00:04:52 +0600

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -235,7 +235,7 @@
 
 
 <!-- This is my swipper section start-->
-<div class="content_data container-fluid"> 
+<div class="content_data container"> 
   <section id="reviews" class="pt-5 mb-2">
     <h3 class="text-center">Reviews Our Clients</h3>
     <div class="swiper">
