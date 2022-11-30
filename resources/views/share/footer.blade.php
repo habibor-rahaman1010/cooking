e7d17db20b13cc363b7c4003b98c0663b3c0ac1d
habibor144369 <onilhabib1@gmail.com>
2022-11-30 21:45:37 +0600

    Hello Programmer

--- resources/views/share/footer.blade.php
+++ resources/views/share/footer.blade.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-	
+{{-- 	
 		<link rel="stylesheet" href="{{secure_asset('assets/css/ionicons.min.css')}}">
-		<link rel="stylesheet" href="{{secure_asset('assets/css/style.css')}}">
+		<link rel="stylesheet" href="{{secure_asset('assets/css/style.css')}}"> --}}
+
+		<link rel="stylesheet" href="{{asset('assets/css/ionicons.min.css')}}">
+		<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
   </head>
   <body>
 		<footer class="footer-06">
