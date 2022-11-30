cd74f23caf5f0e3d858cb4581ee4367f0be5de50
habibor144369 <onilhabib1@gmail.com>
2022-11-30 23:00:21 +0600

    ok

--- resources/views/share/common.blade.php
+++ resources/views/share/common.blade.php
@@ -20,7 +20,7 @@
 
     <script defer src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"
   ></script>
-  <script defer src="{{asset('assets/app.js')}}"></script>
+  <script defer src="{{secure_asset('assets/app.js')}}"></script>
   </head>
 
 
